e1895ec36a32729b20af01b1b4f6cf589d3f7e93
Ariadna Maldonado <ariadnamaldonadogrieco@gmail.com>
2024-03-19 23:39:53 -0300

    Fixed apostrophes

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -104,14 +104,14 @@ const AboutSection = () => {
         />
         <div className="mt-4 md:mt-0 flex flex-col h-full">
           <p className="text-base lg:text-lg">
-            I'm a dedicated FullStack Developer with experience in both Backend
+            I&apos;m a dedicated FullStack Developer with experience in both Backend
             and Frontend. Strongly committed to creating accessible and
             user-centric websites and applications. <br />
             My current focus revolves around using
             <b className="text-xl mx-2">
               React, Bootstrap, Node.js, TypeScript, Tailwind
             </b>
-            (among others) to build dynamic and engaging web applications. Let's
+            (among others) to build dynamic and engaging web applications. Let&apos;s
             connect and collaborate to build immersive digital experiences!
           </p>
           <div className="flex flex-row justify-start mt-8">
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-rose-600 to-pink-500">
-              Hi! I'm {""}
+              Hi! I&apos;m {""}
             </span>
             <br />
             <TypeAnimation
